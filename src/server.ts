5fe422bd51efaad37fa81f3f0c9d5ce225c327e9
Natan Falconi <nfalconi65@gmail.com>
2021-02-23 21:26:58 -0300

    yarn dev

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,12 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { Router } from 'express'
 import './database'
+import { router } from "./routes"
 
 const app = express()
 
-//http://localhost:3333/users
-app.get("/", (req, res) => {
-    return res.json({ message: "ola" })
-})
-
-
-app.post('/', (req, res) => {
-    // recebeu os dados para salvar
-    return res.json({ message: "os dados foram salvos"})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen('3333', () => {
     console.log('server is running')
